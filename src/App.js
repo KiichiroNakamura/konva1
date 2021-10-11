eab76007e38d547c71784b27a22467db1c60df47
KiichiroNakamura <47166584+KiichiroNakamura@users.noreply.github.com>
2021-10-11 10:29:05 +0900

    202110111028

--- src/App.js
+++ src/App.js
@@ -8,6 +8,87 @@ export default function App() {
     const [image] = useImage("https://konvajs.org/assets/yoda.jpg");
     return <Image image={image} x={0} y={0} width={500} height={500} />;
   };
+
+  const PADDING = 5;
+  const HEIGHT = 3000;
+  var WIDTH = 3000;
+
+  const verticalBar = (stage) => {
+    console.log(stage.WIDTH);
+    const dragBoundFuncA = (pos) => {
+      pos.x = stage.width() - PADDING - 10;
+      pos.y = Math.max(
+        Math.min(pos.y, stage.height() - this.height() - PADDING),
+        PADDING
+      );
+      return pos;
+    };
+    const onDragMoveFunc = () => {
+      const availableHeight =
+        stage.height() - PADDING * 2 - verticalBar.height();
+      var delta = (verticalBar.y() - PADDING) / availableHeight;
+
+      stage.Layer.y(-(HEIGHT - stage.height()) * delta);
+    };
+
+    return (
+      <React
+        width={10}
+        height={100}
+        fill={"grey"}
+        opacity={0.8}
+        x={stage.width() - PADDING - 10}
+        y={PADDING}
+        draggable={"true"}
+        dragBoundFunc={dragBoundFuncA(event.target.pos)}
+        onDragMove={onDragMoveFunc}
+      />
+    );
+  };
+
+  const horizontalBar = (stage, pos) => {
+    const dragBoundFuncA = () => {
+      pos.x = Math.max(
+        Math.min(pos.x, stage.width() - this.width() - PADDING),
+        PADDING
+      );
+      pos.y = stage.height() - PADDING - 10;
+
+      return pos;
+    };
+    const dragmoveFunc = () => {
+      const availableWidth =
+        stage.width() - PADDING * 2 - horizontalBar.width();
+      var delta = (horizontalBar.x() - PADDING) / availableWidth;
+
+      stage.layer.x(-(WIDTH - stage.width()) * delta);
+    };
+
+    return (
+      <React
+        width={100}
+        height={10}
+        fill={"grey"}
+        opacity={0.8}
+        x={{ PADDING }}
+        y={stage.height() - PADDING - 10}
+        draggable={"true"}
+        dragBoundFunc={dragBoundFuncA(pos)}
+        dragmove={dragmoveFunc}
+      />
+    );
+  };
+  //scrollLayers.add(verticalBar);
+
+  const Scrolllayer = () => {
+    return (
+      <Layer>
+        <verticalBar />
+        <horizontalBar />
+      </Layer>
+    );
+  };
+
   const Mizutama = () => {
     var WIDTH = 3000;
     var HEIGHT = 3000;
@@ -38,10 +119,11 @@ export default function App() {
         ref={stageRef}
       >
         <Layer width={10} height={50} />
-        <Layer>
+        {/* <Layer>
           <BackImage />
-        </Layer>
+        </Layer> */}
         <Mizutama />
+        <Scrolllayer value={Stage} />
         {/* 何かをするとき、必ずLayerで括る必要がある */}
         <Layer>
           <Rect fill="red" x={100} y={100} width={300} height={200} />
